23e38a5e6818e36d44d7bc4d714b0212eb70966e
shakhbozbekusmonov <miracle_0319@bk.ru>
2024-09-05 10:53:40 +0500

    add: pages

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
       <CarouselContent>
         {Array.from({ length: 2 }).map((_, index) => (
           <CarouselItem key={index}>
-            <section className='h-screen bg-[url("https://files.itv.uz/uploads/helper/2024/06/06//c01744ef18aa3251c621e956c0544cd6-q-1920x750.jpeg")] bg-center bg-cover '></section>
+            <section className='h-screen bg-[url("https://files.itv.uz/uploads/helper/2024/06/06//c01744ef18aa3251c621e956c0544cd6-q-1920x750.jpeg")] bg-center bg-cover'></section>
           </CarouselItem>
         ))}
       </CarouselContent>
