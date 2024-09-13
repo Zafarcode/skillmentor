830371add78a31dba5e0d126163eede9050ada0a
unknown <umarovmuhammad0901@gmail.com>
2024-09-14 02:54:50 +0500

    multfilms detail 00.00.02 version

--- app/multfilms/[slug]/page.tsx
+++ app/multfilms/[slug]/page.tsx
@@ -17,42 +17,42 @@ const FilmsDetail = ({ params: { slug } }: { params: { slug: string } }) => {
 
   return (
     <main
-      className="pt-24"
+      className="pt-14"
       style={{
         backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${selectedFilm?.poster})`,
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
         backgroundPosition: "center",
-        width: "100vw",
+        width: "98.75vw",
         height: "100vh",
       }}
     >
       <div className="container">
-      <Link className="relative flex gap-2 mb-10 text-white" href="/films">
-            <span className="text-3xl">&#8636;</span>
-            <p className="text-xl font-medium">Ortga</p>
-          </Link>
+        <Link className="relative flex gap-2 mb-10 text-white" href="/multfilms">
+          <span className="text-3xl">&#8636;</span>
+          <p className="text-xl font-medium">Ortga</p>
+        </Link>
         <div className="mb-16 grid grid-cols-1 md:grid-cols-2 gap-8 place-items-center">
           <div className="relative rounded-lg overflow-hidden">
             <Image
               src={selectedFilm?.poster}
               alt={selectedFilm.title}
-              width={450} // Kenglikni 200 ga o'rnatish
-              height={550} // Balandlikni 300 ga o'rnatish
-              className="w-[450px] h-[550px] object-cover rounded-lg" // Aniq o'lchamlarni berish
+              width={450} // Kenglikni 450 ga o'rnatish
+              height={550} // Balandlikni 550 ga o'rnatish
+              priority
+              className="object-cover rounded-lg" // Aniq o'lchamlarni berish
             />
-            <Button variant="ghost" className="absolute top-2 text-white p-2 rounded-full ">
+            <Button variant="ghost" className="absolute top-2 right-2 text-white p-2 rounded-full">
               <PlayIcon className="w-6 h-6" />
             </Button>
           </div>
-          <div className="flex flex-col gap-8 text-white ">
-            <h1 className="text-3xl font-bold  ">{selectedFilm.title}</h1>
-            <div className="flex items-center gap-2 ">
+          <div className="flex flex-col gap-8 text-white">
+            <h1 className="text-3xl font-bold">{selectedFilm.title}</h1>
+            <div className="flex items-center gap-2">
               <StarIcon className="w-5 h-5 fill-primary" />
               <span>{selectedFilm.rating}</span>
             </div>
-
-            <div className="flex items-center gap-2 ">
+            <div className="flex items-center gap-2">
               <ClockIcon className="w-5 h-5" />
               <span>142 min</span>
             </div>
@@ -62,46 +62,41 @@ const FilmsDetail = ({ params: { slug } }: { params: { slug: string } }) => {
             </div>
             <div className="flex items-center gap-2 text-gray-500">
               <ListIcon className="w-5 h-5" />
-              <span>{selectedFilm.categories.join(",")}</span>
+              <span>{selectedFilm.categories.join(", ")}</span>
             </div>
-
             <div className="flex items-center gap-4">
               <div className="flex items-center justify-center">
-                <Button
-                  variant="outline"
-                  className="px-4 py-2 bg-gray-200 text-black rounded-md flex items-center space-x-2"
-                >
-                  <span>Treylerni korish</span>
-                  <div className="w-6 h-6 bg-gray-300 flex items-center justify-center rounded-full">
-                    <PlayIcon className="text-black w-4 h-4" />
-                  </div>
-                </Button>
-              </div>
-
-              <Tooltip>
-                <TooltipTrigger>
-                  <Button
+              <Button
                     variant="outline"
-                    className="px-4 py-2 bg-gray-200 text-black rounded-md flex items-center space-x-2"
+                    className="px-4 py-2 bg-sky-400 text-black border-none rounded-md flex items-center space-x-2"
                   >
-                    <span>Sevimli</span>
-                    <div className="w-6 h-6 bg-gray-300 flex items-center justify-center rounded-full">
-                      <HeartIcon className=" text-black w-4 h-4" />
+                    <span>Treylerni korish</span>
+                    <div className="w-6 h-6 bg-white flex items-center justify-center rounded-full">
+                      <PlayIcon className="text-sky-600 fill-sky-600 w-4 h-4" />
                     </div>
                   </Button>
+              </div>
+              <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      variant="outline"
+                      className="px-4 py-2 bg-red-400 border-none text-black rounded-md flex items-center space-x-2"
+                    >
+                      <span>Sevimli</span>
+                      <div className="w-6 h-6 bg-gray-300 flex items-center justify-center rounded-full">
+                        <HeartIcon className="w-4 h-4 text-red-600 fill-red-600" />
+                      </div>
+                    </Button>
+                  </TooltipTrigger>
                   <TooltipContent className="w-40 absolute right-0 bottom-1">
                     <p>Sevimliga qo&apos;shish</p>
                   </TooltipContent>
-                </TooltipTrigger>
-              </Tooltip>
+                </Tooltip>
             </div>
           </div>
         </div>
-        
-
         <div className="pt-[84px]">
           <h2 className="font-bold text-2xl mb-[30px]">O&apos;xshash filmlar</h2>
-
           <section className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
             {multfilm.map((mult) => (
               <SimilarMultfilm key={mult.id} {...mult} />
@@ -114,4 +109,3 @@ const FilmsDetail = ({ params: { slug } }: { params: { slug: string } }) => {
 };
 
 export default FilmsDetail;
-
