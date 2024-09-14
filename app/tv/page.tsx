b00600056d9ce2f87cf3143ea9ea1c2b2841c404
Ravshanovich-R <frondenddevoloper@gmail.com>
2024-09-14 10:33:36 +0500

    tv completed

--- app/tv/page.tsx
+++ app/tv/page.tsx
@@ -1,13 +1,13 @@
-import FilmCard from "@/components/film/film-card";
-import { films } from "@/mock";
+import TvCard from "@/components/tv/tv-card";
+import { tv } from "@/mock";
 
 const Tv = () => {
   return (
     <main className="pt-28 px-5 md:px-0">
       <div className="container">
         <section className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
-          {films.map((film) => (
-            <FilmCard key={film.id} {...film} />
+          {tv.map((tv) => (
+            <TvCard key={tv.id} {...tv} />
           ))}
         </section>
       </div>
