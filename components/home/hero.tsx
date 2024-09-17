768ff7fdd7126ba01ca69e9fbc3680584db3eddd
shakhbozbekusmonov <miracle_0319@bk.ru>
2024-09-17 10:49:28 +0500

    fix: hero y scroll

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
 import { PlayIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
@@ -9,74 +9,65 @@ const Hero = () => {
   const imageUrl = "https://files.itv.uz/uploads/helper/2024/06/06//c01744ef18aa3251c621e956c0544cd6-q-1920x750.jpeg";
 
   return (
-    <div className="relative">
-      <Carousel
-        opts={{
-          loop: true,
-        }}
-        plugins={[
-          Autoplay({
-            delay: 4000,
-          }) as any,
-        ]}
-      >
-        <CarouselContent>
-          {Array.from({ length: 10 }).map((_, index) => (
-            <CarouselItem key={index}>
-              <section className="relative h-screen bg-cover bg-center" style={{ backgroundImage: `url(${imageUrl})` }}>
-                <div className="absolute inset-0 bg-black opacity-60"></div>
+    <Carousel
+      opts={{
+        loop: true,
+      }}
+      plugins={[
+        Autoplay({
+          delay: 4000,
+        }),
+      ]}
+    >
+      <CarouselContent>
+        {Array.from({ length: 10 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <section className="relative h-screen bg-cover bg-center" style={{ backgroundImage: `url(${imageUrl})` }}>
+              <div className="absolute inset-0 bg-black opacity-60"></div>
 
-                <section className="relative pt-80 pl-48 items-center z-10">
-                  <div className="container">
-                    <div className="mb-16 grid grid-cols-1 md:grid-cols-2 items-center">
-                      <div className="absolute flex gap-[680px]">
-                        <div className="space-y-5 mt-28">
-                          <h1 className="text-3xl font-bold text-white">Nomi</h1>
-                          <div className="flex items-center gap-2">
-                            <StarIcon className="w-5 h-5 fill-yellow-400 text-yellow-400" />
-                            <span className="text-white">Rating</span>
-                          </div>
-                          <div className="prose text-white">
-                            <p className="opacity-50">Qisqa Malumoti</p>
-                          </div>
-                          <div className="flex items-center justify-center">
-                            <Button
-                              variant="outline"
-                              className="px-5 py-5 bg-sky-400 text-white font-bold border-none rounded-md flex items-center space-x-2"
-                            >
-                              <span>Ko&apos;proq ko&apos;rish</span>
-                              <PlayIcon className=" fill-white w-5 h-5" />
-                            </Button>
-                          </div>
+              <section className="relative pt-80 pl-48 items-center z-10">
+                <div className="container">
+                  <div className="mb-16 grid grid-cols-1 md:grid-cols-2 items-center">
+                    <div className="absolute flex gap-[680px]">
+                      <div className="space-y-5 mt-28">
+                        <h1 className="text-3xl font-bold text-white">Nomi</h1>
+                        <div className="flex items-center gap-2">
+                          <StarIcon className="w-5 h-5 fill-yellow-400 text-yellow-400" />
+                          <span className="text-white">Rating</span>
                         </div>
+                        <div className="prose text-white">
+                          <p className="opacity-50">Qisqa Malumoti</p>
+                        </div>
+                        <div className="flex items-center justify-center">
+                          <Button
+                            variant="outline"
+                            className="px-5 py-5 bg-sky-400 text-white font-bold border-none rounded-md flex items-center space-x-2"
+                          >
+                            <span>Ko&apos;proq ko&apos;rish</span>
+                            <PlayIcon className=" fill-white w-5 h-5" />
+                          </Button>
+                        </div>
+                      </div>
 
-                        <div className="overflow-hidden">
-                          <Image
-                            src={"/image.png"}
-                            width={300}
-                            height={450}
-                            className="w-60 h-auto object-cover rounded-lg"
-                            style={{ aspectRatio: "300/450", objectFit: "cover" }}
-                            alt="Descriptive image alt text"
-                          />
-                        </div>
+                      <div className="overflow-hidden">
+                        <Image
+                          src={"/image.png"}
+                          width={300}
+                          height={450}
+                          className="w-60 h-auto object-cover rounded-lg"
+                          style={{ aspectRatio: "300/450", objectFit: "cover" }}
+                          alt="Descriptive image alt text"
+                        />
                       </div>
                     </div>
                   </div>
-                </section>
+                </div>
               </section>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-
-        <div className="absolute top-1/2 left-4 transform -translate-y-1/2 z-20">
-          <CarouselPrevious className="text-white" />
-        </div>
-        <div className="absolute top-1/2 right-4 transform -translate-y-1/2 z-20">
-          <CarouselNext className="text-white" />
-        </div>
-      </Carousel>
-    </div>
+            </section>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
   );
 };
 
